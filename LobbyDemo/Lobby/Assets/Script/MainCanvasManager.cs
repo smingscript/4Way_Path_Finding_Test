dc4669539353d8c6ebec035e8ed162c7f4676423
수민 김 <strobe410glitter@gmail.com>
2018-06-20 08:47:11 +0900

    update room listing

---
+++ LobbyDemo/Lobby/Assets/Script/MainCanvasManager.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class MainCanvasManager : MonoBehaviour
+{
+    public static MainCanvasManager Instance;
+    
+    [SerializeField] private LobbyCanvas _lobbyCanvas;
+
+    public LobbyCanvas LobbyCanvas
+    {
+        get { return _lobbyCanvas; }
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void OnClickMakeRoom()
+    {
+        GameObject roomSettingPanel = transform.Find("RoomSettingPanel").gameObject;
+        GameObject lobbyUI = transform.Find("Lobby").gameObject;
+        roomSettingPanel.SetActive(true);
+        lobbyUI.SetActive(false);
+    }
+}
